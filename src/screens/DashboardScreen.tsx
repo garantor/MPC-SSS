e67e786bdec8ae9cf03617e794606d5854be81ab
asunday <asunday@innovative-technology.com>
2026-01-14 11:48:04 +0000

    Add chain prop to Modal component in DashboardScreen

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -161,6 +161,7 @@ export function DashboardScreen({ evmAddress, solanaAddress, onDisconnect }: Das
                 onClose={() => setIsModalOpen(false)}
                 onSuccess={fetchBalance}
                 address={currentAddress}
+                chain={selectedChain}
             />
         </div>
     );
